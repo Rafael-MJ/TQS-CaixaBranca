447bc3ce0e8bffd60fca54fc1146f963c8602d5e
Rafael Modena Jakubovsky <rafaeljakubovsky@gmail.com>
2023-10-04 21:11:52 -0300

    v0.1

--- src/login/User.java
+++ src/login/User.java
@@ -12,23 +12,38 @@ public class User {
             Class.forName("com.mysql.Driver.Manager").newInstance();
             String url = "jdbc:mysql://127.0.0.1/test?user=lopes&password=123";
             conn = DriverManager.getConnection(url);
+
+        //6
         }catch  (Exception e) { }
         return conn;}
+
+    //1
     public String nome="";
     public boolean result = false;
+
+    //2
     public boolean verificarUsuario(String login, String senha) {
         String sql = "";
+
+        //3
         Connection conn = conectarBD();
-        //INSTRUÇÃO SQL
         sql += "select nome from usuarios ";
         sql +="where login = " + "'" + login + "'";
         sql += " and senha = " + "'" + senha + "';";
+
+        //4
         try{
             Statement st = conn.createStatement();
             ResultSet rs = st.executeQuery(sql);
+
+            //5
             if (rs.next()) {
                 result = true;
                 nome = rs.getString("nome");}
+
+        //6
         }catch (Exception e) { }
+
+        //7
         return result; }
-}//fim da class
+}
